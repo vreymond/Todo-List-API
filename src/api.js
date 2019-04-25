b215050cbd505d1978e2b3211444d5e99ab0bf1d
vreymond <valentinreymond@gmail.com>
2019-04-25 09:50:08 +0200

    Commands lines options management to define API port, DB port, username, password

--- src/api.js
+++ src/api.js
@@ -12,12 +12,25 @@ let app = express();
 // Commands options manager
 program
     .version('0.0.1')
-    .option('-p, --port <port>', 'Set port listening', 3000)
+    .option('-p, --portAPI <portAPI>', 'Set API port listening')
+    .option('-P, --portDB <portDB>', 'Set DB port')
+    .option('-u, --dbUser <userDBName>', 'Set DB username')
+    .option('-w, --password <passwordDB>', 'Set DB password')
     .option('-v, --verbosity <logLevel', 'Set log level', setLogLevel)
     .parse(process.argv);
 
+
+// Get logins from commands options manager
+let portAPI, portDB, dbUser, passwordDB;
+
+program.portAPI ? portAPI = program.portAPI : portAPI = 3000;
+program.portDB ? portDB = program.portDB : portDB = 8889;
+program.dbUser ? dbUser = program.dbUser : dbUser = 'root';
+program.password ? passwordDB = program.password : passwordDB = 'root';
+
+
 logger.info(`***** Starting Todo List API *****`);
 
-const PORT = program.port;
+app.listen(portAPI, () => logger.info(`Server listening on port ${portAPI}`));
 
-app.listen(PORT, () => logger.info(`Server listening on port ${PORT}`));
+

fff8813303a5169a59f3de6c653a4bb1277108da
vreymond <valentinreymond@gmail.com>
2019-04-25 09:31:08 +0200

    Adding logger with winston package

--- src/api.js
+++ src/api.js
@@ -1,2 +1,23 @@
+// Packages / modules requirement
 let program = require('commander');
 let express = require('express');
+let mysql = require('mysql');
+let logger = require('./lib/logger').logger;
+let setLogLevel = require('./lib/logger').setLogLevel;
+
+
+let app = express();
+
+
+// Commands options manager
+program
+    .version('0.0.1')
+    .option('-p, --port <port>', 'Set port listening', 3000)
+    .option('-v, --verbosity <logLevel', 'Set log level', setLogLevel)
+    .parse(process.argv);
+
+logger.info(`***** Starting Todo List API *****`);
+
+const PORT = program.port;
+
+app.listen(PORT, () => logger.info(`Server listening on port ${PORT}`));
---
+++ src/lib/logger.js
@@ -0,0 +1,66 @@
+let winston = require('winston');
+
+const config = {
+    levels: {
+        error: 0,
+        debug: 1,
+        warn: 2,
+        data: 3,
+        info: 4,
+        verbose: 5,
+        silly: 6,
+        custom: 7
+    },
+    colors: {
+        error: 'red',
+      debug: 'blue',
+      warn: 'yellow',
+      data: 'grey',
+      info: 'green',
+      verbose: 'cyan',
+      silly: 'magenta',
+      custom: 'yellow'
+    }
+  };
+
+winston.addColors(config.colors);
+
+function createFileTransport(path="./sw-universe.log"){
+
+    let files = new winston.transports.File({ filename: path,
+        format: winston.format.combine(
+        winston.format.simple()
+        ) });
+    return files;
+}
+
+let consoleT = new winston.transports.Console(
+  { 
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.simple()
+    )
+});
+winston.add(consoleT);
+
+
+let logLevel = 'info';
+
+function isLogLvl(value){
+    return config.levels.hasOwnProperty(value)
+}
+function setLogLevel(value) {
+    
+    if(!isLogLvl(value))
+        throw `Unrecognized logLvel "${value}"`;
+    logLevel = value;
+    winston.level = logLevel;
+}
+
+function setLogFile(logFilePath) {
+    let fileCustomTransport = createFileTransport(logFilePath);
+    winston.add(fileCustomTransport);
+}
+
+exports.logger = winston;
+exports.setLogLevel = setLogLevel;

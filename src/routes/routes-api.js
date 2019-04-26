92ebec5d802606ce98da8281ddfd1b093358df6b
vreymond <valentinreymond@gmail.com>
2019-04-26 21:38:40 +0200

    Inserting new Todo in database, and give the response to the client

--- src/routes/routes-api.js
+++ src/routes/routes-api.js
@@ -3,10 +3,11 @@ let jwt = require ('jsonwebtoken');
 let router = express.Router();
 let jsonfile = require('jsonfile');
 let mysql = require('mysql');
+let EventEmitter = require('events');
 
 let logger = require('../lib/logger').logger;
 
-
+const emitter = new EventEmitter();
 let configDBfile = './config-db.json'
 let configDB = jsonfile.readFileSync(configDBfile)
 
@@ -38,8 +39,40 @@ router.get('/lists/all', tokenCheck, (req, res) => {
       });
 });
 
-router.get('/lists/new', (req, res) => {
-    
+router.get('/lists/new/:name', tokenCheck, (req, res) => {
+
+    let nameTodo = req.params.name;
+    let idTodo = 0;
+    jwt.verify(req.token, 'secret_key', (err, data) => {
+        if (err) {
+            res.status(500).send("Unexpected error");
+        }
+        else {
+            
+            db.query(`INSERT INTO TodoProject.List (name) VALUES ('${nameTodo}');`, (err, result, fields) => {
+                if (err) {
+                    logger.error(`An error occured during the "${nameTodo}" todo list creation`);
+                    throw err;
+                }
+               
+                // Get the id of the inserted row
+                idTodo = result.insertId;
+                emitter.emit('NewTodoDone')
+                console.log(idTodo + ' Dans query')
+            })
+            
+
+            emitter.on('NewTodoDone', () => {
+                console.log(idTodo + ' Aprs query')
+                res.status(200).json({
+                    id: idTodo,
+                    name: nameTodo,
+                    nb_tasks: 0
+                });
+            })
+            
+        }
+    });
 })
 
 const getAllList = async () => {
